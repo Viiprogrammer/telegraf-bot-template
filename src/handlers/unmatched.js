34ebf717319ff357bce36278a7c19242f7a2ec6d
Maksim <m_telega@mail.ru>
2021-10-27 03:02:48 +0300

    Added unmatch heandler

---
+++ src/handlers/unmatched.js
@@ -0,0 +1,12 @@
+const unmatchedHandler = async ctx => {
+  ctx.state[unmatchedHandler.unmatched] = true
+  if (ctx.chat && ctx.chat.type === 'private') {
+    await ctx.reply(
+      'Sorry, I couldn\'t understand that, do you need /help?'
+    )
+  }
+}
+
+unmatchedHandler.unmatched = Symbol('unmatchedHandler.unmatched')
+
+module.exports = unmatchedHandler
