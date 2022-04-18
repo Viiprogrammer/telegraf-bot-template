0842e434f287ea1be115ff71311929b221d15607
Maksim <m_telega@mail.ru>
2022-04-19 00:27:15 +0300

    Add stage registration

--- src/bot/handlers/middlewares/index.js
+++ src/bot/handlers/middlewares/index.js
@@ -1,10 +1,11 @@
-const { Composer } = require('telegraf')
+const { Composer, Stage } = require('telegraf')
 const composer = new Composer()
 
 composer.use(
   require('./session')(),
   require('./authorization'),
   require('./parseArguments'),
+  new Stage(require('../../scenes')),
   Composer.privateChat(require('./adminComposer'))
 )
 module.exports = composer
--- src/bot/scenes/index.js
+++ src/bot/scenes/index.js
@@ -1,10 +1,7 @@
 const path = require('path')
 const fs = require('fs')
-const dir = fs.readdirSync(path.join(__dirname, path.sep))
 
-dir.forEach(function (filename) {
-  if (path.extname(filename) === '.js' && filename !== 'models.js') {
-    const exportAsName = path.basename(filename)
-    module.exports[exportAsName] = require(path.join(__dirname, filename))
-  }
-})
+module.exports = fs
+  .readdirSync(__dirname)
+  .filter(file => !['index.js'].includes(file) && fs.lstatSync(path.join(__dirname, file)).isFile())
+  .map(file => require(path.join(__dirname, file)))
