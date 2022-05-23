b65fc18481d1c5e9b93e78fe8cfb71eba06b8472
Maksim <m_telega@mail.ru>
2022-05-23 14:12:17 +0300

    Fix file name filter

--- src/bot/handlers/commands/index.js
+++ src/bot/handlers/commands/index.js
@@ -5,7 +5,7 @@ const router = new Router(require('./router'))
 
 router.handlers = new Map(fs
   .readdirSync(__dirname)
-  .filter(file => !['bootstrap.js', 'router.js'].includes(file) && fs.lstatSync(path.join(__dirname, file)).isFile())
+  .filter(file => !['index.js', 'router.js'].includes(file) && fs.lstatSync(path.join(__dirname, file)).isFile())
   .map(file => [path.parse(file).name.toLowerCase(), require(path.join(__dirname, file))]))
 
 module.exports = router
