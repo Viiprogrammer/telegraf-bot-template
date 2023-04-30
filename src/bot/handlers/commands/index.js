3092df28b8605180eb745226713a51b558b71b2a
viiprogrammer <m_telega@mail.ru>
2023-04-30 17:42:55 +0300

    chore(auto-export): remove this shit

--- src/bot/handlers/commands/index.js
+++ src/bot/handlers/commands/index.js
@@ -1,11 +1,7 @@
 const { Router } = require('telegraf')
-const path = require('path')
-const fs = require('fs')
 const router = new Router(require('./router'))
 
-router.handlers = new Map(fs
-  .readdirSync(__dirname)
-  .filter(file => !['index.js', 'router.js'].includes(file) && fs.lstatSync(path.join(__dirname, file)).isFile())
-  .map(file => [path.parse(file).name.toLowerCase(), require(path.join(__dirname, file))]))
+router.handlers = new Map()
+router.handlers.set('help', require('./help'))
 
 module.exports = router
--- src/bot/scenes/index.js
+++ src/bot/scenes/index.js
@@ -1,7 +1,1 @@
-const path = require('path')
-const fs = require('fs')
-
-module.exports = fs
-  .readdirSync(__dirname)
-  .filter(file => !['index.js'].includes(file) && fs.lstatSync(path.join(__dirname, file)).isFile())
-  .map(file => require(path.join(__dirname, file)))
+module.exports = {}
